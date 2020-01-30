c259fbceb820bdd6d4c5109250394689161c2aee
kursivee <kursivee.dev@gmail.com>
2020-01-30 07:24:27 -0500

    Reattempt context impl with scheduler

--- src/main/kotlin/co/naiveimpl/SimpleCoWithContext.kt
+++ src/main/kotlin/co/naiveimpl/SimpleCoWithContext.kt
@@ -5,50 +5,68 @@ package co.naiveimpl
  */
 fun main() {
 
-    val C1 = "C1"
-    val C2 = "C2"
+    /**
+     // Scheduler
+     wait 10ms then toggle isSuspended = false
 
-    val context = Context(
-        mapOf(C1 to 10000000.0, C2 to 100000.0),
-        mutableMapOf(C1 to 0.0, C2 to 0.0),
-        mutableMapOf(C1 to false, C2 to false)
-    )
-
-    while(context.hasNext()) {
-        launch(context, C1) {
-            if(it.counts[C1] != 0.0) {
-                it.flags[C1] = true
-                println("C1 done")
-            }
+     // Coroutine
+     while(!context.isComplete) {
+        context.schedule()
+        launch(context.child) {
+            // do stuff
         }
-        launch(context, C2) {
-            if(it.counts[C2] != 0.0) {
-                it.flags[C2] = true
-                println("C2 done")
+     }
+
+     // launch(context) {
+        if(!context.isSuspended) {
+            // doStuff
+        }
+     }
+
+     */
+
+    val child = Context()
+    val parent = Context()
+
+    parent.child = child
+    while(!parent.complete) {
+        Scheduler.schedule(listOf(parent.child!!))
+        if(!child.suspended) {
+            println("DO STUFF")
+            child.finish()
+        }
+    }
+
+}
+
+object Scheduler {
+    private const val timeSlice: Long = 5000
+
+    fun schedule(contexts: List<Context>) {
+        contexts.forEach { context ->
+            if(context.suspendTime == -1L) {
+                context.suspendTime = System.currentTimeMillis()
+                context.suspended = true
+            } else {
+                context.suspended = System.currentTimeMillis() - context.suspendTime <= timeSlice
             }
         }
     }
-    println("Completed")
 }
 
-fun launch(context: Context, tag: String, lambda: (Context) -> Unit) {
-    if(!context.isComplete(tag)) {
-        if(context.counts[tag] == 0.0) {
-            lambda(context)
-            context.counts[tag] = context.counts[tag]!! + 1
-        } else if(context.counts[tag]!! >= context.limits[tag]!!) {
-            lambda(context)
-        } else {
-            context.counts[tag] = context.counts[tag]!! + .08
+class Context {
+    var complete = false
+    var suspendTime: Long = -1
+    var suspended = false
+    var parent: Context? = null
+    var child: Context? = null
+        set(value) {
+            value!!.parent = this
+            field = value
         }
-    }
-}
 
-data class Context(
-    val limits: Map<String, Double>,
-    val counts: MutableMap<String, Double>,
-    val flags: MutableMap<String, Boolean>
-) {
-    fun hasNext(): Boolean = flags.filter { !it.value }.isNotEmpty()
-    fun isComplete(tag: String): Boolean = flags[tag] ?: true
+    fun finish() {
+        complete = true
+        parent?.complete = true
+    }
 }
